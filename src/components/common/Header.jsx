9475b09e4946c7770dbe29c5c0120a39487cadd7
Thanh Nguyen <nguyen.hathanh0810@gmail.com>
2023-08-15 16:59:17 +0700

    chore: update responsive header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,54 +1,83 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
-import React from "react"
+import React, { useState } from "react"
 import IconPhone from "../icons/IconPhone"
 import IconEnvelop from "../icons/IconEnvelop"
 import IconFacebook from "../icons/IconFacebook"
 import IconInstagram from "../icons/IconInstagram"
 import IconXTwitter from "../icons/IconXTwitter"
 import LogoUrl from "@public/logo.png"
+import { Italianno } from "next/font/google"
+import IconBars from "../icons/IconBars"
+import SideMenu from "../portals/SideMenu"
+
+const italianno = Italianno({ subsets: ["latin"], weight: "400" })
 
 function Header() {
+  const [sideMenuOpen, setSideMenuOpen] = useState(false)
+
   return (
-    <header className="fixed inset-x-0 xl:px-[60px]">
-      <div className="flex h-10 border-b border-[#ffffff80] text-white">
-        <div className="flex flex-grow gap-6 items-center">
+    <header className="absolute inset-x-0 px-4 xl:px-[60px]">
+      <div
+        className={
+          "hidden text-white md:flex md:h-10 mmd:border-b md:border-[#ffffff80" +
+          " " +
+          italianno.className
+        }
+      >
+        <div className="flex flex-grow gap-8 items-center text-[20px]">
           <div className="flex items-center">
-            <span className="mr-1">
-              <IconPhone size={20} />
+            <span className="mr-2">
+              <IconPhone size={14} />
             </span>
-            <Link href="tel:+84355848720">
-              <i>(+84) 355848720</i>
+            <Link href="tel:+84379797979">
+              <span className="tracking-wider">(+84) 379797979</span>
             </Link>
           </div>
           <div className="flex items-center">
-            <span className="mr-1">
-              <IconEnvelop size={20} />
+            <span className="mr-2">
+              <IconEnvelop size={14} />
             </span>
             <Link href="mailto:cruise-angency@gmail.com">
-              <i>cruise-agency@gmail.com</i>
+              <span className="tracking-wider">cruise-agency@gmail.com</span>
             </Link>
           </div>
         </div>
         <div className="flex flex-shrink-0 gap-4 items-center">
-          <i className="text-lg">Socials</i>
+          <i className="mr-2 text-[22px] tracking-wider">Socials</i>
           <Link href="#">
-            <IconFacebook size={20} />
+            <IconFacebook size={14} />
           </Link>
           <Link href="#">
-            <IconInstagram size={20} />
+            <IconInstagram size={14} />
           </Link>
           <Link href="#">
-            <IconXTwitter size={20} />
+            <IconXTwitter size={14} />
           </Link>
         </div>
       </div>
-      <nav className="py-4 border-b border-[#ffffff80] text-white">
-        <ul className="flex gap-12 items-center justify-center">
-          <li>
+      <nav className="text-white py-4 md:border-b md:border-[#ffffff80]">
+        <ul className="flex gap-12 items-center justify-center relative">
+          <li className="absolute left-6 md:hidden">
+            <button
+              type="button"
+              className="select-none"
+              onClick={() => setSideMenuOpen(true)}
+            >
+              <IconBars size={24} />
+            </button>
+            {sideMenuOpen === true ? (
+              <SideMenu
+                isOpen={sideMenuOpen}
+                onClose={() => setSideMenuOpen(false)}
+              />
+            ) : null}
+          </li>
+          <li className="hidden md:list-item">
             <Link href="#">
               <span className="uppercase text-lg font-semibold">
-                Our Routes
+                DESTINATIONS
               </span>
             </Link>
           </li>
@@ -57,10 +86,10 @@ function Header() {
               <Image src={LogoUrl} alt="Cruise Agency" width={105} />
             </Link>
           </li>
-          <li>
+          <li className="hidden md:list-item">
             <Link href="#">
               <span className="uppercase text-lg font-semibold">
-                Contact Us
+                CONTACT US
               </span>
             </Link>
           </li>
---
+++ src/components/portals/SideMenu.jsx
@@ -0,0 +1,139 @@
+"use client"
+import LogoUrl from "@public/logo.png"
+import Image from "next/image"
+import Link from "next/link"
+import { createPortal } from "react-dom"
+import IconBoat from "../icons/IconBoat"
+import IconContact from "../icons/IconContact"
+import { Italianno } from "next/font/google"
+import IconPhone from "../icons/IconPhone"
+import IconEnvelop from "../icons/IconEnvelop"
+import IconFacebook from "../icons/IconFacebook"
+import IconXTwitter from "../icons/IconXTwitter"
+import IconInstagram from "../icons/IconInstagram"
+import { useEffect } from "react"
+import IconAngleLeft from "../icons/IconAngleLeft"
+
+const italianno = Italianno({ subsets: ["latin"], weight: "400" })
+
+function SideMenu({ isOpen, onClose }) {
+  useEffect(() => {
+    const [$body, $html] = [document.body, document.body.parentElement]
+    const [bodyOverflow, htmlOverflow] = [
+      $body.style.overflow,
+      $html.style.overflow,
+    ]
+    $body.style.overflow = "hidden"
+    $html.style.overflow = "hidden"
+
+    return function () {
+      $body.style.overflow = bodyOverflow
+      $html.style.overflow = htmlOverflow
+    }
+  }, [])
+
+  if (!isOpen) return null
+
+  function handleClose() {
+    onClose?.()
+  }
+
+  function handleClick(e) {
+    e.stopPropagation()
+  }
+
+  const SideBar = () => (
+    <aside
+      className="fixed inset-0 z-[100] bg-[#00000080]"
+      onClick={handleClose}
+    >
+      <nav
+        className="min-w-[320px] w-[75%] h-full flex flex-col bg-white shadow-md"
+        onClick={handleClick}
+      >
+        <div className="pt-2">
+          <button
+            type="button"
+            className="block ml-auto mr-2 p-2 select-none text-slate-400"
+            onClick={handleClose}
+          >
+            <IconAngleLeft size={24} />
+          </button>
+        </div>
+        <div className="flex-shrink-0 flex justify-center py-4">
+          <Link href="#" className="">
+            <Image src={LogoUrl} alt="Cruise Agency" width={96} />
+          </Link>
+        </div>
+        <ul className="flex-grow p-4">
+          <li className="pl-0 py-4 flex justify-center">
+            <Link href="#" className="flex items-center gap-2 px-3">
+              <IconBoat size={16} />
+              <span className="uppercase text-lg font-semibold">
+                DESTINATIONS
+              </span>
+            </Link>
+          </li>
+          <li className="pl-0 py-4 flex justify-center">
+            <Link href="#" className="flex items-center gap-2 px-3">
+              <IconContact size={16} />
+              <span className="uppercase text-lg font-semibold">
+                CONTACT US
+              </span>
+            </Link>
+          </li>
+        </ul>
+        <ul
+          className={
+            "flex-shrink-0 pb-4 text-[20px]" + " " + italianno.className
+          }
+        >
+          <li className="w-[25%] mx-auto mb-4 border-b border-primary-shades-by-3">
+            {" "}
+          </li>
+          <li className="flex justify-center py-1">
+            <Link href="tel:+84379797979" className="flex items-center gap-2">
+              <IconPhone size={16} />
+              <span className="tracking-wider">
+                Phone/WhatsApp +84379797979
+              </span>
+            </Link>
+          </li>
+          <li className="flex justify-center py-1">
+            <Link
+              href="mailto:cruise-angency@gmail.com"
+              className="flex items-center gap-2"
+            >
+              <IconEnvelop size={16} />
+              <span className="tracking-wider">
+                Mailbox cruise-angency@gmail.com
+              </span>
+            </Link>
+          </li>
+          <li className="flex justify-center py-1">
+            <Link href="#" className="flex items-center gap-2">
+              <IconFacebook size={16} />
+              <span className="tracking-wider">Fanpage</span>
+            </Link>
+          </li>
+          <li className="flex justify-center py-1">
+            <Link href="#" className="flex items-center gap-2">
+              <IconXTwitter size={16} />
+              <span className="tracking-wider">Twitter X</span>
+            </Link>
+          </li>
+          <li className="flex justify-center py-1">
+            <Link href="#" className="flex items-center gap-2">
+              <IconInstagram size={16} />
+              <span className="tracking-wider">Instagram</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  )
+
+  return createPortal(<SideBar />, document.body)
+}
+
+export default SideMenu
